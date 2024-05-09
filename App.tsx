9dcfdd602850c996bfef243b03c2736d461e22fd
randy1burrell <rb@randyburrell.info>
2024-05-09 18:57:21 -0500

    feat: Add geo locationing
    This commit adds react-native-geolocation.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,28 @@
+import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import BackgroundGeolocation  from 'react-native-background-geolocation'
 
 export default function App() {
+  useEffect(() => {
+    BackgroundGeolocation.ready({
+      desiredAccuracy: BackgroundGeolocation.DESIRED_ACCURACY_HIGH,
+      distanceFilter: 10,
+      stopOnTerminate: false,
+      startOnBoot: true,
+      debug: true,
+      url: 'https://localhost:3000',
+      params: {
+        user_id: 123,
+      },
+      headers: {
+        'my-auth-token': 'secret-key',
+      },
+    }).then((state) => {
+      console.log(`${state} ready`);
+      BackgroundGeolocation.start();
+    });
+  });
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
